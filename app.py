9dd55ddd29f4ce596f4cbd96879dcf268d36c7ef
Paulo <paulomr2000@gmail.com>
2025-12-15 20:49:18 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+Microsoft Windows [Version 10.0.26100.7462]
+(c) Microsoft Corporation. All rights reserved.
+
+(base) C:\Users\PAULO\Carpeta de Trabajo\stramlit_test>(base) C:\Users\PAULO\Carpeta de Trabajo\stramlit_test> Volume in drive C is OS
+ Volume Serial Number is B245-E9B4
+
+ Directory of C:\Users\PAULO\Carpeta de Trabajo\stramlit_test
+
+12/15/2025  08:09 PM    <DIR>          .
+12/15/2025  07:43 PM    <DIR>          ..
+12/15/2025  07:43 PM             4,895 .gitignore
+12/15/2025  08:09 PM                 0 app.py
+12/15/2025  07:43 PM                15 README.md
+12/15/2025  07:53 PM                24 requeriments.txt
+               4 File(s)          4,934 bytes
+               2 Dir(s)  93,967,044,608 bytes free
+
+(base) C:\Users\PAULO\Carpeta de Trabajo\stramlit_test> Volume in drive C is OS
+ Volume Serial Number is B245-E9B4
+
+ Directory of C:\Users\PAULO\Carpeta de Trabajo\stramlit_test
+
+12/15/2025  08:09 PM    <DIR>          .
+12/15/2025  07:43 PM    <DIR>          ..
+12/15/2025  07:43 PM             4,895 .gitignore
+12/15/2025  08:09 PM                 0 app.py
+12/15/2025  07:43 PM                15 README.md
+12/15/2025  07:53 PM                24 requeriments.txt
+               4 File(s)          4,934 bytes
+               2 Dir(s)  93,967,032,320 bytes free
+
+(base) C:\Users\PAULO\Carpeta de Trabajo\stramlit_test>
+
+import streamlit as st
+import pandas as pd
+import scipy as sp
+
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
+
+git add .
+git commit -m "initial commit"
+git push
