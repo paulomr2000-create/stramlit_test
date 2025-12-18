78b1b798946c6a64d20e8cb53bcf43208d463965
Paulo <paulomr2000@gmail.com>
2025-12-18 13:46:49 -0800

    Se cambia requeriments por requirements

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ st.header('Lanzar una moneda')
 
 st.write('Esta aplicación aún no es funcional. En construcción.')
 
-git add .
-git commit -m "initial commit"
-git push
+#git add .
+#git commit -m "initial commit"
+#git push
